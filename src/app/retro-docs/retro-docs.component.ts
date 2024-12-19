01431f5d89ba6d9382e3d6d73ba0777dc922bebd
Marcel Menke <marcel.menke1981@gmail.com>
2024-12-19 09:23:17 +0100

    added style to retro docs

--- src/app/retro-docs/retro-docs.component.ts
+++ src/app/retro-docs/retro-docs.component.ts
@@ -3,10 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-retro-docs',
   standalone: true,
-  imports: [],
   templateUrl: './retro-docs.component.html',
   styleUrl: './retro-docs.component.scss'
 })
 export class RetroDocsComponent {
-
 }
